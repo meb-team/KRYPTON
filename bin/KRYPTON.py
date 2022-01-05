295d3efd99fb64e844f1b9464a6aaea58ec97839
Damien Courtine <damien.courtine@uca.fr>
2022-01-05 15:51:51 +0100

    more cleaning, follow PEP8 rules
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

--- bin/KRYPTON.py
+++ bin/KRYPTON.py
@@ -14,15 +14,20 @@ Damien
 Tasks to do:
     - [ ] Use the module argparse for the parameters
 
-    - [ ] use "with open(xxx, ?) as yyy" to read files. It is much more efficient
+    - [ ] use "with open(xxx, ?) as yyy" to read files.
+        It is much more efficient
 __authors__ = ['bmilisavljevic', 'AnthonyAUCLAIR', 'd-courtine']
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Run the pipeline KRYPTON, for Transcriptome assembly and annotation') #setup the parser
-parser.add_argument('--bucketin', help='Name of the bucket used to read data from', required=True)
-parser.add_argument('--bucketout', help='Name of the bucket used to store data in', required=True)
-parser.add_argument(['--mode', '-m'], help='Mode for the pipeline, between "reads", "assembly" and "cds".'
+parser = argparse.ArgumentParser(description='Run the pipeline KRYPTON, \
+            for Transcriptome assembly and annotation')  # setup the parser
+parser.add_argument('--bucketin', help='Name of the bucket used to read \
+            data from', required=True)
+parser.add_argument('--bucketout', help='Name of the bucket used to store\
+            data in', required=True)
+parser.add_argument(['--mode', '-m'], help='Mode for the pipeline, between \
+            "reads", "assembly" and "cds".'
     default = "reads", choices = ['reads', 'assembly', 'cds'])
 kryp = parser.parse_args()
 
@@ -33,7 +38,8 @@ kryp = parser.parse_args()
 
 """
 # On active un environnement conda avec python3.5 installe prealablement
-#os.system("PATHCONDA=$(conda info | grep -i 'base environment' | awk -F" " '{print $4}')")
+#os.system("PATHCONDA=$(conda info | grep -i 'base environment' \
+            | awk -F" " '{print $4}')")
 #os.system("source $PATHCONDA'/etc/profile.d/conda.sh'")
 #os.system("conda activate snakes")
 #os.system("source activate snakes")
@@ -81,7 +87,8 @@ def Creation_dossier(nom_dossier):
 def Check_etape(fichier_teste, commande):
     # va verifier si une etape a deja ete realise ou non
     try:
-        process = Popen(['ls {}'.format(fichier_teste)], stdout=PIPE, stderr=PIPE, shell=True)
+        process = Popen(['ls {}'.format(fichier_teste)], stdout=PIPE,
+                        stderr=PIPE, shell=True)
         stdout, stderr = process.communicate()
         stdout = stdout.rstrip()
         stdout = stdout.decode("utf-8")
@@ -94,13 +101,14 @@ def Check_etape(fichier_teste, commande):
 def print_time_used(time_start, time_end, step_name):
     """This function prints the time taken by the system to run a step"""
     time_min = int((time_end - time_start) // 60)  # get the minutes
-    time_sec = math.trunc((time_end - time_start) % 60)  # get the number of seconds
+    time_sec = math.trunc((time_end - time_start) % 60)  # get the seconds
     print("%s: %smin %ssec" % (step_name, time_min, time_sec))
 
 
 # ########## main ##########
 
-print("Bienvenue sur le pipeline Krypton, la totalité des étapes risque de prendre du temps soyez patient.")
+print("Bienvenue sur le pipeline KRYPTON, la totalité des étapes \
+        risque de prendre du temps soyez patient.")
 debut_time_Global = time.time()
 directory_KRYPTON = Path(__file__).parent
 Creation_dossier(dir_output)
@@ -109,24 +117,27 @@ if mode_pipeline == "reads":
     # ########## Partie assemblage ##########
     # ########## fastqc ##########
     """
-    Integrate this step into a function as it is used twice. Also make sure to control all
-    parameters for fastqc, like the number of CPUs.
+    Integrate this step into a function as it is used twice. Also make sure
+    to control all parameters for fastqc, like the number of CPUs.
     """
-    print("Debut de l etape de fastqc sur les reads, cette etape peut prendre du temps, patience.")
+    print("Debut de l etape de fastqc sur les reads, cette etape peut \
+            prendre du temps, patience.")
     debut_timefastqc_raw = time.time()
     # définit un dossier de sortie pour fastqc
     output_fastqc_raw = "fastqc_raw"
     # creer le dossier de sortie et s'y deplace dedans
     Creation_dossier(output_fastqc_raw)
     fichier_cible = "*1_1*.zip"
-    commande = "fastqc {} {} --outdir ./ --threads 15 > raw_fastqc.log 2>&1".format(read_forward, read_backward)
+    commande = "fastqc {} {} --outdir ./ --threads 15 \
+                >raw_fastqc.log 2>&1".format(read_forward, read_backward)
     Check_etape(fichier_cible, commande)
     fin_timefastqc_raw = time.time()
 
     print_time_used(debut_timefastqc_raw, fin_timefastqc_raw, "FastQC_Raw")
 
     # ########## trimmomatic ##########
-    print("Debut de l etape de nettoyage avec trimmomatic des reads, cette etape peut prendre du temps, patience.")
+    print("Debut de l etape de nettoyage avec trimmomatic des reads,\
+            cette etape peut prendre du temps, patience.")
     debut_timeTrim = time.time()
     # permet de revenir au dossier de resultats
     os.chdir(dir_output)
@@ -135,7 +146,13 @@ if mode_pipeline == "reads":
     # creer le dossier de sortie et s'y deplace dedans
     Creation_dossier(output_trimmomatic)
     fichier_cible = "forward*.paired.fastq"
-    commande = "java -jar /opt/apps/trimmomatic-0.33/trimmomatic-0.33.jar PE {} {} forward.trimmomatic.paired.fastq forward.trimmomatic.unpaired.fastq reverse.trimmomatic.paired.fastq reverse.trimmomatic.unpaired.fastq MINLEN:32 SLIDINGWINDOW:10:20 LEADING:5 TRAILING:5 > trimmomatic.log 2>&1".format(read_forward, read_backward)
+    commande = "java -jar /opt/apps/trimmomatic-0.33/trimmomatic-0.33.jar\
+            PE {} {} forward.trimmomatic.paired.fastq\
+            forward.trimmomatic.unpaired.fastq\
+            reverse.trimmomatic.paired.fastq\
+            reverse.trimmomatic.unpaired.fastq MINLEN:32 SLIDINGWINDOW:10:20\
+            LEADING:5 TRAILING:5 > trimmomatic.log 2>&1"\
+            .format(read_forward, read_backward)
 
     Check_etape(fichier_cible, commande)
     fin_timeTrim = time.time()
@@ -148,7 +165,8 @@ if mode_pipeline == "reads":
     path_trim_backward = os.path.abspath(trim_backward)
 
     # ########## trim_fastqc ##########
-    print("Debut de l etape de fastqc sur les reads nettoyes par Trimmomatic, cette etape peut prendre du temps, patience.")
+    print("Debut de l etape de fastqc sur les reads nettoyes par Trimmomatic,\
+            cette etape peut prendre du temps, patience.")
     debut_timefastqc_Trim = time.time()
 
     # permet de revenir au dossier de resultats
@@ -159,7 +177,8 @@ if mode_pipeline == "reads":
     # creer le dossier de sortie et s'y deplace dedans
     Creation_dossier(output_fastqc_trimmed)
     fichier_cible = "forward*.zip"
-    commande = "fastqc {} {} --outdir ./ --threads 15 > trim_fastqc.log 2>&1".format(path_trim_forward, path_trim_backward)
+    commande = "fastqc {} {} --outdir ./ --threads 15 > trim_fastqc.log 2>&1".\
+        format(path_trim_forward, path_trim_backward)
 
     Check_etape(fichier_cible, commande)
     fin_timefastqc_Trim = time.time()
@@ -168,7 +187,8 @@ if mode_pipeline == "reads":
 
     if assembly_mode == "trinity":
         # ########## Trinity ##########
-        print("Debut de l etape d'assemblage sur les reads nettoyes par Trimmomatic, cette etape est longue, patience !!!")
+        print("Debut de l etape d'assemblage sur les reads nettoyes \
+            par Trimmomatic, cette etape est longue, patience !!!")
         debut_timeTrinity = time.time()
         # permet de revenir au dossier de resultats
         os.chdir(dir_output)
@@ -179,7 +199,10 @@ if mode_pipeline == "reads":
         Creation_dossier(output_trinity)
 
         fichier_cible = "Trinity.fasta"
-        commande = "Trinity --seqType fq --left {} --right {} --output ../trinity_out --CPU 20 --max_memory 95G > trinity.log 2>&1".format(path_trim_forward, path_trim_backward)
+        commande = "Trinity --seqType fq --left {} --right {} --output\
+            ../trinity_out --CPU 20 --max_memory 95G > trinity.log 2>&1"\
+            .format(path_trim_forward, path_trim_backward)
+
         Check_etape(fichier_cible, commande)
         fin_timeTrinity = time.time()
         print_time_used(debut_timeTrinity, fin_timeTrinity, "Trinity_assembly")
@@ -196,7 +219,8 @@ if mode_pipeline == "assembly" or mode_pipeline == "reads":
     output_clust = "mmseqs2_Trans_clust"
     Creation_dossier(output_clust)
     fichier_cible = "clusterRes_cluster.tsv"
-    commande = "mmseqs easy-linclust {} clusterRes tmp > cluster.log 2>&1".format(path_trinity)
+    commande = "mmseqs easy-linclust {} clusterRes tmp > cluster.log 2>&1"\
+        .format(path_trinity)
     Check_etape(fichier_cible, commande)
 
     path_clust = os.path.abspath("clusterRes_rep_seq.fasta")
@@ -206,7 +230,8 @@ if mode_pipeline == "assembly" or mode_pipeline == "reads":
 
 # permet de revenir au dossier de resultats
 os.chdir(dir_output)
-print("Debut de l etape de traduction des transcrits apres une clusterisation en proteines, cette etape peut prendre du temps, patience.")
+print("Debut de l etape de traduction des transcrits apres une clusterisation\
+        en proteines, cette etape peut prendre du temps, patience.")
 debut_timeTansdecoder = time.time()
 
 output_transdecoder = "Transdecoder"
@@ -214,12 +239,16 @@ Creation_dossier(output_transdecoder)
 if mode_pipeline == "assembly" or mode_pipeline == "reads":
     os.system("TransDecoder.LongOrfs -m 30 -t {}".format(path_clust))
     os.system("TransDecoder.Predict -t {}".format(path_clust))
-    os.system("python {}/modifi_format.py clusterRes_rep_seq.fasta.transdecoder.pep > clusterpep.fasta 2>&1".format(directory_KRYPTON))
+    os.system("python {}/modifi_format.py\
+            clusterRes_rep_seq.fasta.transdecoder.pep >\
+            clusterpep.fasta 2>&1".format(directory_KRYPTON))
 
 if mode_pipeline == "cds":
     os.system("TransDecoder.LongOrfs -m 30 -t {} 2>&1".format(path_trinity))
     os.system("TransDecoder.Predict -t {} 2>&1".format(path_trinity))
-    os.system("python {}/modifi_format.py assembly.fasta.transdecoder.pep > clusterpep.fasta 2>&1".format(directory_KRYPTON))
+    os.system("python {}/modifi_format.py \
+            assembly.fasta.transdecoder.pep > clusterpep.fasta 2>&1\
+            ".format(directory_KRYPTON))
 
 fin_timeTansdecoder = time.time()
 print_time_used(debut_timeTansdecoder, fin_timeTansdecoder, "Transdecoder")
