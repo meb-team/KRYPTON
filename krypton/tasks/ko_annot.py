ca49b6ea59f233ae835d49fc4a4555b33e7fd40b
Damien Courtine <damien.courtine@uca.fr>
2022-02-01 13:40:01 +0100

    KOfamscan result parsing seem OK. Now MetaPathExplorer
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

--- krypton/tasks/ko_annot.py
+++ krypton/tasks/ko_annot.py
@@ -19,21 +19,47 @@ def ko_check_user_files(file_path):
 
 
 class KO_annot():
-    def __init__(self, threads=None, project=None, ko_files=None,
-                 proteins=None):
+    def __init__(self, threads=1, project=None, ko_files=None,
+                 proteins=None, new_dir=True):
         self.max_threads = threads
         self.output = project + "/" + '09_ko_annot'
         self.input = proteins
-        self.ko_list = ko_files + '/ko_list'
-        self.profiles = ko_files + '/profiles'
+        self.ko_list = '' if not ko_files else ko_files + '/ko_list'
+        self.profiles = '' if not ko_files else ko_files + '/profiles'
         self.tmp = self.output + "/tmp"
-        u.create_dir(self.output)
+        self.results = self.output + '/09_kofam_results.tsv'
+        if new_dir:
+            u.create_dir(self.output)
 
     def run_kofamscan(self, format=None, step=None):
-        command = f'exec_annotation -o {self.output}/09_kofam_results.tsv ' +\
+
+        command = f'exec_annotation -o {self.results} ' +\
                   f'--format {format} --ko-list {self.ko_list} --profile ' +\
                   f'{self.profiles} --cpu {self.max_threads} ' +\
                   f'--tmp-dir {self.tmp} {self.input}'
         with open(f"{self.output}/09_kofam_logs.log", "w") as log:
             u.run_command(command, log=log, step=step)
         return True
+
+    def parse_results(self):
+        """Extract the significative KOs from the result of  `run_kofamscan()`
+        """
+        result_d = dict()
+        with open(self.results, 'r') as fi:
+            lines = fi.readlines()
+            for line in lines:
+                line = line.rstrip().split('\t')
+                if line[0] == "*":
+                    if line[2] not in result_d.keys():
+                        result_d[line[2]] = 1
+                    else:
+                        result_d[line[2]] += 1
+        with open(self.results + '.matrix_MPE.tsv', 'w') as fo:
+            for k, v in result_d.items():
+                print(k, v, sep='\t', file=fo)
+        return True
+
+
+if __name__ == '__main__':
+    test = KO_annot(project='damien/test_30', new_dir=False)
+    test.parse_results()
