09a9b3f8f39bef919e8358062b1465edc0f9d8b1
Damien Courtine <damien.courtine@uca.fr>
2022-02-04 10:13:06 +0100

    add a script to download K0/pathways from KEGG API
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

---
+++ bin/download_KEGG_data.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+import requests
+import time
+
+"""
+The code present in this script is heavily inspired by a Nolwen's blog
+post from bioinfo-fr.net!
+
+https://bioinfo-fr.net/jouer-lapi-de-kegg
+
+The KEGG API: https://www.kegg.jp/kegg/rest/keggapi.html
+Links that will be used in this script
+    - http://rest.kegg.jp/list/pathway ==> all maps and their name
+    - http://rest.kegg.jp/list/ko ==> all K0 and their name
+    - http://rest.kegg.jp/link/pathway/ko ==> K0 to map and ko
+"""
+
+pathways = "http://rest.kegg.jp/list/pathway"
+K0 = "http://rest.kegg.jp/list/ko"
+pathways_to_K0 = "http://rest.kegg.jp/link/pathway/ko"
+
+# ## Pathways
+r = requests.get(pathways)
+r_out = 'ressources/KEGG_pathways.tsv'
+print(f"Download the list of pathways from KEGG in {r_out}")
+with open(r_out, "w") as fo:
+    lines = r.text.splitlines()
+    for line in lines:
+        """
+        From "path:map00010	Glycolysis / Gluconeogenesis", get two items:
+            - name: map00010
+            - fct: Glycolysis / Gluconeogenesis
+        """
+        name = line.split("\t")[0].split(":")[1]
+        fct = line.split("\t")[1]
+        print(name, fct, sep="\t", file=fo)
+
+time.sleep(2)  # wait 2 seconds
+
+# ## K0s
+s = requests.get(K0)
+s_out = 'ressources/KEGG_K0.tsv'
+print(f"Download the list of K0s from KEGG in {s_out}")
+with open(s_out, "w") as fo:
+    lines = s.text.splitlines()
+    for line in lines:
+        name = line.split("\t")[0].split(":")[1]
+        fct = line.split("\t")[1]
+        print(name, fct, sep="\t", file=fo)
+
+time.sleep(2)
+
+# ## Get the link between pathway and K0
+t = requests.get(pathways_to_K0)
+t_out = 'ressources/KEGG_K0_to_pathway.tsv'
+print(f"Download the links between pathways and K0s from KEGG in {t_out}")
+with open(t_out, "w") as fo:
+    lines = t.text.splitlines()
+    for line in lines:
+        ko = line.split("\t")[0].split(":")[1]
+        pathway = line.split("\t")[1].split(":")[1]
+        print(ko, pathway, sep="\t", file=fo)
+
+print("All data were downloaded from KEGG.")
