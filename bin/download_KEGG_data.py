9e7e2cab24a442387cf13bad5736f42d46ff0b82
Damien Courtine <damien.courtine@uca.fr>
2022-02-10 17:14:40 +0100

    update code to download info from KEGG - WIP
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

--- bin/download_KEGG_data.py
+++ bin/download_KEGG_data.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+import os
 import requests
 import time
+import urllib.request
+
+import krypton.utils as u
 
 """
 The code present in this script is heavily inspired by a Nolwen's blog
@@ -21,9 +25,17 @@ pathways = "http://rest.kegg.jp/list/pathway"
 K0 = "http://rest.kegg.jp/list/ko"
 pathways_to_K0 = "http://rest.kegg.jp/link/pathway/ko"
 
+# # Setup the output directory
+out = os.path.dirname(os.path.abspath(__name__)) + '/ressources/KEGG_data/'
+try:
+    u.check_dir_exists(out)
+except Exception:
+    u.create_dir(out)
+
+# # Download the data
 # ## Pathways
 r = requests.get(pathways)
-r_out = 'ressources/KEGG_pathways.tsv'
+r_out = out + 'KEGG_pathways.tsv'
 print(f"Download the list of pathways from KEGG in {r_out}")
 with open(r_out, "w") as fo:
     lines = r.text.splitlines()
@@ -41,7 +53,7 @@ time.sleep(2)  # wait 2 seconds
 
 # ## K0s
 s = requests.get(K0)
-s_out = 'ressources/KEGG_K0.tsv'
+s_out = out + 'KEGG_K0.tsv'
 print(f"Download the list of K0s from KEGG in {s_out}")
 with open(s_out, "w") as fo:
     lines = s.text.splitlines()
@@ -54,7 +66,7 @@ time.sleep(2)
 
 # ## Get the link between pathway and K0
 t = requests.get(pathways_to_K0)
-t_out = 'ressources/KEGG_K0_to_pathway.tsv'
+t_out = out + 'KEGG_K0_to_pathway.tsv'
 print(f"Download the links between pathways and K0s from KEGG in {t_out}")
 with open(t_out, "w") as fo:
     lines = t.text.splitlines()
@@ -65,3 +77,10 @@ with open(t_out, "w") as fo:
             print(ko, pathway, sep="\t", file=fo)
 
 print("All data were downloaded from KEGG.")
+
+# # ### Data to run KOfamScan
+#
+# url_base = 'https://www.genome.jp/ftp/db/kofam/'
+# for file in ['ko_list.gz', 'profiles.tar.gz']:
+#     urllib.request.urlretrieve(url_base + file, out + file)
+#     time.sleep(2)
