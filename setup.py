0c4ee12ebbdf3b4228614061138bd6637a54ef24
Damien Courtine <damien.courtine@uca.fr>
2022-02-15 11:52:35 +0100

    update setup.py
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import sys
-from setuptools import setup
+import glob
+from setuptools import setup, find_packages
 
 try:
     if sys.version_info.major != 3:
@@ -12,8 +13,8 @@ except Exception:
 
 setup(
     name='krypton',
-    version="0.1.0",
-    packages=['krypton'],
-    scripts=['bin/KRYPTON.py'],
+    version="0.2.0",
+    packages=find_packages(where="krypton"),
+    scripts=[script for script in glob.glob('bin/*.py')],
     license="GPLv3+",
 )
